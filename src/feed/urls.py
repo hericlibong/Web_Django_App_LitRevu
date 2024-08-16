d31f3fb3cb7e02ad652ac83a3f39a813815e58d1
hericlibong <hericlibong@gmail.com>
2024-08-16 20:35:22 +0200

    add connexion confirm

--- src/feed/urls.py
+++ src/feed/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TicketCreateView
+from .views import TicketCreateView, TicketListView, TicketUpdateView, TicketDeleteView
 
 
 app_name = 'feed'
 
 urlpatterns = [
     path('add_ticket/', TicketCreateView.as_view(), name='add_ticket'),
+    path('my_tickets/', TicketListView.as_view(), name='ticket_list'),
+    path('edit_ticket/<int:pk>/', TicketUpdateView.as_view(), name='edit_ticket'),
+    path('delete_ticket/<int:pk>/', TicketDeleteView.as_view(), name='delete_ticket'),
+
 ]
--- src/feed/views.py
+++ src/feed/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from .models import Ticket
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 class TicketCreateView(LoginRequiredMixin, CreateView):
+    """View for creating a ticket."""
     model = Ticket
     fields = ['title', 'description', 'image']
     template_name = 'feed/ticket_create.html'
@@ -13,5 +15,45 @@ class TicketCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+
+class TicketUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """View for updating a ticket."""
+    model = Ticket
+    fields = ['title', 'description', 'image']
+    template_name = 'feed/ticket_update_form.html'
+    success_url = reverse_lazy('feed:ticket_list')
+
+    def test_func(self):
+        ticket = self.get_object()
+        return ticket.user == self.request.user
+    
+
+class TicketDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """View for deleting a ticket."""
+    model = Ticket
+    template_name = 'feed/ticket_confirm_delete.html'
+    success_url = reverse_lazy('feed:ticket_list')
+
+    def test_func(self):
+        ticket = self.get_object()
+        return ticket.user == self.request.user
+    
+
+class TicketListView(LoginRequiredMixin, ListView):
+    """View for listing tickets."""
+    model = Ticket
+    template_name = 'feed/ticket_list.html'
+    context_object_name = 'tickets'
+    ordering = ['-time_created']
+    
+    def get_queryset(self):
+        return Ticket.objects.filter(user=self.request.user)
+    
+
+
+    
+
+
 
 
