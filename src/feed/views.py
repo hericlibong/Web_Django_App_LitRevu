2c579304cf649ba84571d8e9dbd10b0f02882643
hericlibong <hericlibong@gmail.com>
2024-08-18 04:43:52 +0200

    added review view

--- src/feed/views.py
+++ src/feed/views.py
@@ -1,8 +1,54 @@
+from django.db.models.query import QuerySet
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
-from django.urls import reverse_lazy
-from .models import Ticket
+from django.urls import reverse, reverse_lazy
+from .models import Ticket, Review
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    """View for creating a review."""
+    model = Review
+    fields = ['ticket','rating', 'headline', 'body']
+    template_name = 'feed/review_create.html'
+
+    def form_valid(self, form):
+        """Set the user of the review to the current user."""
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Return the URL to redirect to after creating a review."""
+        return reverse('feed:ticket_list', kwargs={'pk': self.object.ticket.pk})
+
+
+class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """View for updating a review"""
+    model = Review
+    fields = ['rating', 'headline', 'body']
+    template_name = 'feed/review_update_form.html'
+
+    def get_queryset(self):
+        """Return the reviews of the current user."""
+        return Review.objects.filter(user=self.request.user)
+
+    def get_success_url(self):
+        """Return the URL to redirect to after updating a review."""
+        return reverse('feed:ticket_list', kwargs={'pk': self.object.ticket.pk})
+
+
+class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """View for deleting a review."""
+    model = Review  
+    template_name = 'feed/review_confirm_delete.html'
+
+    def get_queryset(self):
+        """Return the reviews of the current user."""
+        return Review.objects.filter(user=self.request.user)
+
+    def get_success_url(self):
+        """Return the URL to redirect to after deleting a review."""
+        return reverse('feed:ticket_list', kwargs={'pk': self.object.ticket.pk})
 
 
 class TicketCreateView(LoginRequiredMixin, CreateView):
