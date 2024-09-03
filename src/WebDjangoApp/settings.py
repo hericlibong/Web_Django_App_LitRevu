c267d5a5bc56bc0892c5ed423634b9dc81f450e0
hericlibong <hericlibong@gmail.com>
2024-09-03 17:14:18 +0200

    add python-environement

--- src/WebDjangoApp/settings.py
+++ src/WebDjangoApp/settings.py
@@ -12,21 +12,26 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(env_file= str(BASE_DIR / "WebDjangoApp" / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j%l+(ev(p6j#nz^r_9s%51loe4hkh+5n6mpp7uaqq^+r5yc+('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+
 
 
 # Application definition
