10b1aa174c38efbceaae6c6b1121f14ea39abf3e
héric Libong <hericlibong@gmail.com>
2025-01-13 09:12:12 +0100

    prepa profile

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import User
 
 
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'bio')
+
 admin.site.register(User)
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,17 @@
 # account/models.py
 from django.contrib.auth.models import AbstractUser
+# from django.db import models
+# from django.contrib.auth import get_user_model
 
+# User = get_user_model()
 
 class User(AbstractUser):
     pass
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     bio = models.TextField(blank=True, help_text="Une courte biographie de l'utilisateur.")
+#     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, help_text="Image de profil de l'utilisateur.")
+
+#     def __str__(self):
+#         return f"Profil de {self.user.username}"
